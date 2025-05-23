f749b31571094588d410a2f42a1fd906742b7def
Abdallah El-Hakawati <165614371+iHakawaTi@users.noreply.github.com>
2025-05-23 18:01:14 +0300

    Update face_mesh_module.py

--- face_mesh_module.py
+++ face_mesh_module.py
@@ -4,6 +4,17 @@ import time
 
 
 class FaceMeshDetector():
+    """
+    Detects facial landmarks using MediaPipe's Face Mesh.
+
+    Args:
+        staticMode (bool): If True, treats input images as static.
+        maxFaces (int): Max number of faces to detect.
+        refine_landmarks (bool): Whether to refine landmark locations.
+        minDetectionCon (float): Minimum confidence for initial detection.
+        minTrackCon (float): Minimum confidence for tracking landmarks.
+    """
+
     def __init__(self, staticMode=False, maxFaces=2, refine_landmarks=False, minDetectionCon=0.5, minTrackCon=0.5):
 
         self.staticMode = staticMode
@@ -11,13 +22,23 @@ class FaceMeshDetector():
         self.refine_landmarks = refine_landmarks
         self.minDetectionCon = minDetectionCon
         self.minTrackCon = minTrackCon
-
         self.mpDraw = mp.solutions.drawing_utils
         self.mpFaceMesh = mp.solutions.face_mesh
         self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFaces, self.refine_landmarks, self.minDetectionCon, self.minTrackCon)
         self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
     def findFaceMesh(self, img, draw=True):
+        """
+        Finds face meshes in the input image.
+
+        Args:
+            img (ndarray): Input BGR image.
+            draw (bool): Whether to draw mesh on image.
+
+        Returns:
+            img (ndarray): Image with or without drawings.
+            faces (list): List of faces with landmark coordinates.
+        """
         self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.faceMesh.process(self.imgRGB)
         faces = []
